657dfb22486a378d31d855b11136c3835de71532
trybuildstuff <93ventures@gmail.com>
2018-12-19 02:42:54 +0200

    implement time time feature

--- WeatherForcastExecutor.php
+++ WeatherForcastExecutor.php
@@ -1,17 +1,25 @@
 <?php
+date_default_timezone_set('Australia/Adelaide');
+
 class WeatherForcastExecutor
 {
     private $API_URL = 'http://magicseaweed.com/api/06c6c6502c517fa06b1d405326fa2a24/forecast/?units=uk&spot_id=';
-    
+
     private $locations = array(
-      0 => array(
-        'id' => 546,
-        'name' => 'Moffat Beach'
-      ),
-      1 => array(
-        'id' => 1008,
-        'name' => 'Alex Heads'
-      )
+        0 => array(
+            'id' => 546,
+            'name' => 'Moffat Beach',
+            'tideTime' => '',
+            'tideTimeHeight' => '',
+            'key' => 0,
+        ),
+        1 => array(
+            'id' => 1008,
+            'name' => 'Alex Heads',
+            'tideTime' => '',
+            'tideTimeHeight' => '',
+            'key' => 1,
+        ),
     );
 
     private $WIND_DEFINITION = array(
@@ -27,122 +35,153 @@ class WeatherForcastExecutor
         '75,87' => 'Strong Gale',
         '88,102' => 'Storm',
         '103,115' => 'Violent Storm',
-        '116,99999' => 'Hurricane'
+        '116,99999' => 'Hurricane',
     );
 
-    private function initSolidRatingAsHtml($solidRate){
-      $imgsHtml = '';
+    private function initSolidRatingAsHtml($solidRate)
+    {
+        $imgsHtml = '';
 
-      // Loop the solid rating on a single forecast object.
-      for ($i = 0; $i < $solidRate; $i++) {
-        $imgsHtml .= '<img src="imgs/star_filled.png" />';
-      }
+        // Loop the solid rating on a single forecast object.
+        for ($i = 0; $i < $solidRate; $i++) {
+            $imgsHtml .= '<img src="imgs/star_filled.png" />';
+        }
 
-      return $imgsHtml;
+        return $imgsHtml;
     }
 
-    private function initFadedRateAsHtml($fadedRate) {
-      $imgsHtml = '';
+    private function initFadedRateAsHtml($fadedRate)
+    {
+        $imgsHtml = '';
 
-      // Loop the faded rating on a single forecast object.
-      for ($i = 0; $i < $fadedRate; $i++) {
-        $imgsHtml .= '<img src="imgs/star_empty.png" />';
-      }
+        // Loop the faded rating on a single forecast object.
+        for ($i = 0; $i < $fadedRate; $i++) {
+            $imgsHtml .= '<img src="imgs/star_empty.png" />';
+        }
 
-      return $imgsHtml;
+        return $imgsHtml;
     }
 
     private function getWindDescriptionByKMpH($kMpH)
     {
         foreach ($this->WIND_DEFINITION as $key => $value) {
-          $minCutPos = strpos($key, ',');
-          $min = substr($key, 0, $minCutPos);
-          $max = substr($key, $minCutPos + 1, strlen($key));
-          if (in_array($kMpH, range($min, $max))) {
-              return $value;
-          }
+            $minCutPos = strpos($key, ',');
+            $min = substr($key, 0, $minCutPos);
+            $max = substr($key, $minCutPos + 1, strlen($key));
+            if (in_array($kMpH, range($min, $max))) {
+                return $value;
+            }
         }
     }
 
-    private function initRequestUrl($location) {
-      $url = $this->API_URL . $location['id'];
-      return $url;
+    private function initRequestUrl($location)
+    {
+        $url = $this->API_URL . $location['id'];
+        return $url;
     }
 
-    private function getAPIData($location){
-      $requestUrl = $this->initRequestUrl($location);
-      // init CURL      
-      $ch = curl_init();
-      curl_setopt($ch, CURLOPT_URL, $requestUrl);
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-      $output = curl_exec($ch);
-      $curl_error = curl_error($ch);
-      curl_close($ch);
+    private function getAPIData($location)
+    {
+        $requestUrl = $this->initRequestUrl($location);
+        // init CURL
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $requestUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $output = curl_exec($ch);
+        $curl_error = curl_error($ch);
+        curl_close($ch);
 
-      $output = json_decode($output);
-      return $output;
+        $output = json_decode($output);
+        return $output;
     }
 
-    function roundUpToAny($n,$x=10) {
-        return round(($n+$x/2)/$x)*$x;
+    public function roundUpToAny($n, $x = 10)
+    {
+        return round(($n + $x / 2) / $x) * $x;
     }
-    
-    private function getHtmlForLocation($location){
-      
-      $dataObject = $this->getAPIData($location);
 
-      $html = '';
+    private function fillLocationBreakingHeightInfo($locationKey, $locationData)
+    {
+        $maxTideTimeHeight = 0;
 
-      $locationName = $location['name'];
-      
-      foreach ($dataObject as $val) {
-          
-          $swellUnit = $val->swell->unit;
-          $swellDirection = $this->roundUpToAny($val->swell->components->combined->direction);
-          $swellCompassDirection = $val->swell->components->combined->compassDirection;
-          
-          $windDirection = $val->wind->direction;
-          $windKpHSpeed = round(1.609344 * $val->wind->speed);
-          $windDescription = $this->getWindDescriptionByKMpH($windKpHSpeed);
-          
-          $localTime = date('g:i a', $val->localTimestamp);
+        for ($i = 0; $i < count($locationData); $i++) {
+            $locationMaxBH = $locationData[$i]->swell->maxBreakingHeight;
+            if ($locationMaxBH >= $maxTideTimeHeight) {
+                $maxTideTimeHeight = $locationMaxBH;
+                $this->location[$locationKey]['tideTimeHeight'] = $locationMaxBH;
+                $this->location[$locationKey]['tideTime'] = $locationData[$i]->timestamp;
+            }
+        }
 
-          $weatherTemp = $val->condition->temperature;
-          $weatherState = $val->condition->weather;
+        return $this->location[$locationKey];
+    }
 
-          $fadedRating = $this->initFadedRateAsHtml($val->fadedRating);
-          $solidRating = $this->initSolidRatingAsHtml($val->solidRating);
+    private function filterDataBasedOnTime($locationData)
+    {
+        $australiaTime = time();
 
-          $html .= "<tr class=''>
+        for ($i = 0; $i < count($locationData); $i++) {
+            if ($australiaTime <= $locationData[$i]->timestamp ||
+                ($australiaTime >= $locationData[$i]->timestamp &&
+                    $australiaTime <= $locationData[$i + 1]->timestamp)
+            ) {
+                return $locationData[$i];
+            }
+        }
+    }
+
+    private function getHtmlForLocation($location)
+    {
+        $html = '';
+        $locationName = $location['name'];
+        $dataObject = $this->getAPIData($location);
+
+        $val = $this->filterDataBasedOnTime($dataObject);
+        $location = $this->fillLocationBreakingHeightInfo($location['key'], $dataObject);
+
+        $swellUnit = $val->swell->unit;
+        $swellDirection = $this->roundUpToAny($val->swell->components->combined->direction);
+        $swellCompassDirection = $val->swell->components->combined->compassDirection;
+
+        $windDirection = $val->wind->direction;
+        $windKpHSpeed = round(1.609344 * $val->wind->speed);
+        $windDescription = $this->getWindDescriptionByKMpH($windKpHSpeed);
+
+        $weatherTemp = $val->condition->temperature;
+        $weatherState = $val->condition->weather;
+
+        $fadedRating = $this->initFadedRateAsHtml($val->fadedRating);
+        $solidRating = $this->initSolidRatingAsHtml($val->solidRating);
+
+        $html .= "<tr class=''>
                       <td class='text' data-title='Location'>" . $locationName . "</td>
                       <td class='text' data-title='Surf Height'>" . $val->swell->minBreakingHeight . $swellUnit . " - " . $val->swell->maxBreakingHeight . $swellUnit . " </td>
-                      <td class='text' data-title='Tide Times'>3.7ft at " . $localTime . "</td>
+                      <td class='text' data-title='Tide Times'> $location[tideTimeHeight]$swellUnit at " . date('g:i a', $location['tideTime']) . "</td>
                       <td class='text' data-title='Wind Direction'><div class='msw-ssa msw-ssa-$windDirection'></div> " . $windDescription . " " . $val->wind->compassDirection . "</td>
                       <td class='text' data-title='Swells'><div class='msw-swa msw-swa-$swellDirection'></div> " . $swellCompassDirection . "</td>
                       <td class='text' data-title='Weather'>$weatherTemp&deg; <div class='msw-sw msw-sw-$weatherState'></div> </td>
                       <td class='text' data-title='Surf Rating'>$fadedRating $solidRating</td>
                    </tr>";
-      
-      }
 
-      return $html;
+        return $html;
     }
 
     public function __constructor()
     {
-        
+
     }
 
-    public function getExecutedJob(){
-      $locationsHTML = '';
+    public function getExecutedJob()
+    {
+        $locationsHTML = '';
 
-      foreach($this->locations as $location){
-        $locationsHTML .= $this->getHtmlForLocation($location); 
-      }
+        foreach ($this->locations as $location) {
+            $locationsHTML .= $this->getHtmlForLocation($location);
+        }
 
-      return $locationsHTML;
+        return $locationsHTML;
     }
 
 }
